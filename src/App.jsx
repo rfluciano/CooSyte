8f14c3c4b8a6fb8da36dac870bbfc201d95afbcb
rfluciano <lucianorabearimanana@gmail.com>
2025-06-01 14:22:11 +0300

    Initial commit from local project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to React</h1>
+      <p>This is a simple React application.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+<ul class="elements-bar right">
+        <li>
+            <div class="clb-social vc_hidden-md vc_hidden-sm vc_hidden-xs">
+                <ul class="clb-social-holder font-titles icons">
+                    <li class="clb-social-holder-follow">Follow Us</li>
+                    <li class="clb-social-holder-dash">&ndash;</li>
+                    <li>
+                        <a target="_blank" href=" https://www.tiktok.com/@almadigital" class="tiktok">
+                                <i class='fab fa-tiktok'></i>                             </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href=" https://www.linkedin.com/company/almadigitalit" class="linkedin">
+                                <i class='fab fa-linkedin'></i>                             </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href=" https://www.instagram.com/almadigitalit/" class="instagram">
+                                <i class='fab fa-instagram'></i>                             </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href=" https://www.facebook.com/almadigitalit/" class="facebook">
+                                <i class='fab fa-facebook-f'></i>                             </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+    </ul>
